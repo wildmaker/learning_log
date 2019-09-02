b18db2883d6b125751567e34f856d3d7d264d07d
wildmaker <zhang.outsider@gmail.com>
2019-09-02 21:31:30 +0800

    init

---
+++ learning_logs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from learning_logs.models import Topic
+
+admin.site.register(Topic)
+
+from learning_logs.models import Entry
+
+admin.site.register(Entry)

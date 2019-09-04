4e59839748fc53446c52114bd08b2acbccef0882
wildmaker <zhang.outsider@gmail.com>
2019-09-04 22:00:44 +0800

    create app

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     # my apps
     'learning_logs',
 ]
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+# from . import learning_logs
 
 urlpatterns = [
+    path('', include(('learning_logs.urls', 'learning_logs'), namespace = 'learning_logs')),
     path('admin/', admin.site.urls),
 ]

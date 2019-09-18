306611cd2bf36e711cde7c420a64b0c93f5e3c27
wildmaker <zhang.outsider@gmail.com>
2019-09-18 20:12:43 +0800

    add topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,5 +2,13 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    # 主页
     re_path(r'^$', views.index, name = "index"),
+
+    # 显示所有主题
+    re_path(r'^topics/$', views.topics, name = "topics"),
+
+    # 特定主题的详细页面
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -5,3 +5,20 @@ from django.shortcuts import render
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
+
+from .models import Topic
+
+def topics(request):
+    """显示所有主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """显示主题下的所有条目"""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-data_added')
+    context = {'topic':topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
+

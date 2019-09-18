ffd55bb9b6fb2cb619e4e41f8cdddb17922a655a
wildmaker <zhang.outsider@gmail.com>
2019-09-18 21:50:20 +0800

    add entries items

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -17,7 +17,7 @@ def topics(request):
 def topic(request, topic_id):
     """显示主题下的所有条目"""
     topic = Topic.objects.get(id = topic_id)
-    entries = topic.entry_set.order_by('-data_added')
+    entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 

140b247d101e9c6388af5029fa07fa9896e5d7e3
wildmaker <zhang.outsider@gmail.com>
2019-09-24 09:44:21 +0800

    add forms.py

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     # 特定主题的详细页面
     re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
 
+    # 用于添加新主题的网页
+    re_path(r'^new_topic/$', views.new_topic, name = 'new_topic'),
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
@@ -21,4 +23,19 @@ def topic(request, topic_id):
     context = {'topic':topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+from .forms import TopicForm
 
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        # 未提交数据: 创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据, 对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)

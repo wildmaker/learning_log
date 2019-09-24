5b53cd315d76d2a26e86072b4e4d141e547e32d0
wildmaker <zhang.outsider@gmail.com>
2019-09-24 21:46:45 +0800

    allow user to add a new entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     # 用于添加新主题的网页
     re_path(r'^new_topic/$', views.new_topic, name = 'new_topic'),
 
+    # 用于添加新条目的页面
+    re_path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name = 'new_entry')
 ]
